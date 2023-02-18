29de33701202c2370dc66973e8f37d57f17e7941
Serge Volynets <sergej-volynec@yandex.ru>
2023-02-19 01:35:23 +0300

    HTSeminar3+Additional

---
+++ TASK4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите сумму вклада число: ");
+int sum = Convert.ToInt32(Console.ReadLine());
+
+if (sum < 0)
+{
+    Console.WriteLine("Неверный формат числа! Число должно быть положительным.");
+    return;
+}
+
+double result = 0;
+if (sum < 100) result = sum + sum * 0.05;
+else if (sum >= 100 && sum <= 200) result = sum + sum * 0.07;
+else result = sum + sum * 0.1;
+
+Console.Write("Сумма с процентами равна: " + result);
---
+++ TASK5/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите число: ");
+string numberArray = Console.ReadLine();
+
+if (int.TryParse(numberArray, out int isNumber) == false)
+{
+    Console.WriteLine("Некорректиный формат числа!");
+    return;
+}
+
+int size = numberArray.Length;
+
+//Если четное число знаков или один знак, число не имеет средней цифры, поэтому сразу прервемся
+if (size % 2 == 0 || size == 1)
+{
+    Console.WriteLine("Число неинтересное!");
+    return;
+}
+
+//Инициализируем max, min, среднюю цифру
+int.TryParse(Convert.ToString(numberArray[0]), out int max);
+int.TryParse(Convert.ToString(numberArray[0]), out int min);
+int.TryParse(Convert.ToString(numberArray[size / 2]), out int middleDigit);
+
+//Найдем max, min
+for (int i = 1; i < size; i++)
+{
+    int.TryParse(Convert.ToString(numberArray[i]), out int digit);
+    if (digit > max) max = digit;
+    if (digit < min) min = digit;
+}
+
+if (max - min == middleDigit) Console.WriteLine("Число интересное!");
+else Console.WriteLine("Число неинтересное!");
